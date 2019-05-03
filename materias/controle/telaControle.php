7a6832196384b8bc4f81f2ad2bd94b4c82864fb0
Leandro <leandro.crispimmagalhaes@hotmail.com>
2019-05-03 18:53:47 -0300

    Ajustes

--- materias/controle/telaControle.php
+++ materias/controle/telaControle.php
@@ -3,11 +3,11 @@
     @$pagina = $_GET['pagina'];   
 
     switch($pagina){
-        case "cursos":      include "paginas/cursos.php"; break;
-        case "materias":    include "paginas/materias.php"; break;
-        case "prova":       include "paginas/provas.php"; break;
-        case "questoes":    include "paginas/questoes.php"; break;
-        case "usuario":     include "paginas/usuarios.php"; break;
+        case "cursos":      include "paginas/cursos/cursos.php"; break;
+        case "materias":    include "paginas/materias/materias.php"; break;
+        case "prova":       include "paginas/provas/provas.php"; break;
+        case "questoes":    include "paginas/questoes/questoes.php"; break;
+        case "usuario":     include "paginas/usuarios/usuarios.php"; break;
         default:            include "paginas/home.php"; break;
     }
 
