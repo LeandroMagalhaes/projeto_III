a169912d198b6ccddf857da95015352bf151c9e4
Leandro <leandro.crispimmagalhaes@hotmail.com>
2019-05-08 18:45:01 -0300

    Ajustes

--- controle/gradeControle.php
+++ controle/gradeControle.php
@@ -6,8 +6,28 @@
     @$idCurso = $_POST['idCurso'];
     @$idMateria = $_POST['idMateria'];    
     @$acao = $_GET['acao'];
+    
+    if($acao == 'materia'){  
+        
+        @$cod_curso = $_GET['cod_curso'];
 
-    if($acao == 'listar'){  
+        $sql = "SELECT DISTINCT ma.cod_materia, ma.nome_materia FROM materia AS ma INNER JOIN grade AS gr ON gr.cod_materia = ma.cod_materia WHERE gr.cod_curso IN (" . $cod_curso . ")";
+
+        $consultar = new Conexao;
+        $resultado = $consultar->getDados($sql);
+        echo @json_encode($resultado);
+
+    } 
+    else if($acao == 'cursos'){  
+
+        $sql = "SELECT DISTINCT cr.cod_curso, cr.nome_curso FROM curso AS cr INNER JOIN grade AS gr ON cr.cod_curso = gr.cod_curso";
+
+        $consultar = new Conexao;
+        $resultado = $consultar->getDados($sql);
+        echo @json_encode($resultado);
+
+    } 
+    else if($acao == 'listar'){  
 
         $sql = "SELECT cr.cod_curso, cr.nome_curso, ma.cod_materia, ma.nome_materia FROM curso AS cr INNER JOIN grade AS gr ON cr.cod_curso = gr.cod_curso INNER JOIN materia AS ma ON gr.cod_materia = ma.cod_materia";
 
--- paginas/grade/grade - listar.php
+++ paginas/grade/grade - listar.php
@@ -3,52 +3,54 @@
         <h5><label>Grades</label></h5><br>
     </div>
 </div>
-<div class="accordion" id="grade"></div>
+<div class="accordion" id="grade">
+    
+</div>
 
 <script>
 
     $(document).ready(function getGrade() {
-        var xhttp = new XMLHttpRequest();
+        
+        var cursos = [];
+        var html = "";
+        var i = 0;
+        $.get("controle/gradeControle.php?acao=cursos", (data, status) => {
+            var cursos = JSON.parse(data);
+            html = "<div class='card'><div class='card-header' id='1'><h2 class='mb-0'><button class='btn btn-link btn-sm' data-toggle='collapse' data-target='#collapse_1' aria-expanded='true'>"+cursos[i].nome_curso+"</button></h2></div><div id='collapse_1' class='collapse' aria-labelledby='1' data-parent='#grade'><div class='card-body' id='materias_1'></div></div></div>"            
+            cursos.forEach((curso) => {
+                $.get("controle/gradeControle.php?acao=materia&cod_curso=" + curso.cod_curso, (data, status) => {
+                    cursos[curso.cod_curso] = JSON.parse(data);
+                });
+            });
+            console.log(html);
+        });
 
-        xhttp.onreadystatechange = function() {
+        // var xhttp = new XMLHttpRequest();
 
-            if(this.readyState == 4 && this.status == 200) {
-                resultado = JSON.parse(this.responseText);            
-                    p = 1;
-                    materias = 1;
-                    lista = '';
-                    novoCurso = null;                    
+        // xhttp.onreadystatechange = function() {
 
-                    for(var i = 0; i < resultado.length; i++){                    
-                        if(novoCurso == resultado[i].cod_curso){
-                            materias++; 
-                            continue;
-                        }
-                        else{
-                            lista += "<div class='card'><div class='card-header' id='"+ resultado[i].cod_curso +"'><h2 class='mb-0'><button class='btn btn-link btn-sm' data-toggle='collapse' data-target='#collapse_"+p+"' aria-expanded='true' aria-controls='"+ p +"l'>"+ resultado[i].nome_curso +"</button></h2></div><div id='collapse_"+ p +"' class='collapse' aria-labelledby='"+ resultado[i].cod_curso +"' data-parent='#grade'><div class='card-body'>";
-                            
-                            for(var j = 0; j < resultado.length; j++){
-                                /*
-                                if(curso == resultado[i].cod_curso){
-                                    
-                                }else{
-                                    break;
-                                }
-                                */
-                                lista += resultado[j].nome_materia +"<br>";
-                            }
-                            lista += "</div></div></div>";
-                        }
-                        p++;
-                        novoCurso = resultado[i].cod_curso;
-                    }               
-                    console.log(lista);
-                    
-                document.getElementById('grade').innerHTML = lista;            
-            }
-        };
-        xhttp.open("GET", "controle/gradeControle.php?acao=listar", true);
-        xhttp.send();
+        //     if(this.readyState == 4 && this.status == 200) {
+        //         resultado = JSON.parse(this.responseText);            
+        //             p = 1;
+        //             materias = 1;
+        //             lista = '';
+        //             novoCurso = null;                    
+        //             console.log(resultado);
+        //             for(var i = 0; i < resultado.length; i++){                    
+        //                 if(novoCurso == resultado[i].cod_curso){
+        //                     continue;
+        //                 }
+        //                 else{
+        //                     lista += "<div class='card'><div class='card-header' id='"+ resultado[i].cod_curso +"'><h2 class='mb-0'><button class='btn btn-link btn-sm' data-toggle='collapse' data-target='#collapse_"+p+"' aria-expanded='true' aria-controls='"+ p +"l'>"+ resultado[i].nome_curso +"</button></h2></div><div id='collapse_"+ p +"' class='collapse' aria-labelledby='"+ resultado[i].cod_curso +"' data-parent='#grade'></div></div>";
+        //                     }
+        //                 p++;
+        //                 novoCurso = resultado[i].cod_curso;
+        //             }                    
+        //         document.getElementById('grade').innerHTML = lista;            
+        //     }
+        // };
+        // xhttp.open("GET", "controle/gradeControle.php?acao=listar", true);
+        // xhttp.send();
     });
 
     function excluirGrade(id) {
